d54ab0280281c65fb14228a2b5d131083b54b4bf
Bibek Panda <rajendra21101999@gmail.com>
2020-04-18 12:19:38 +0530

    Update Journal.java

--- app/src/main/java/model/Journal.java
+++ app/src/main/java/model/Journal.java
@@ -9,17 +9,50 @@ public class Journal {
     private String username;
     private String userId;
     private String imageUrl;
-
+    private  String documentId;
 
     public Journal() {
     }
 
-    public Journal(String title, String thought, Timestamp date, String username, String userId, String imageUrl) {
+    public Journal(String title, String thought, Timestamp date, String username, String userId, String imageUrl,String documentId) {
         this.title = title;
         this.thought = thought;
         this.date = date;
         this.username = username;
         this.userId = userId;
+        this.imageUrl = imageUrl;
+        this.documentId=documentId;
+    }
+
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setThought(String thought) {
+        this.thought = thought;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setImageUrl(String imageUrl) {
         this.imageUrl = imageUrl;
     }
 
@@ -27,47 +60,23 @@ public class Journal {
         return date;
     }
 
-    public void setDate(Timestamp date) {
-        this.date = date;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
     public String getTitle() {
         return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
     }
 
     public String getThought() {
         return thought;
     }
 
-    public void setThought(String thought) {
-        this.thought = thought;
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
     }
 }
